b6c268c59c80831fce781d78e2c3835196b35f94
Göran Roseen <goran.roseen@acando.com>
2019-01-08 17:46:35 +0100

    Working nicely with https://github.com/marmelab/json-graphql-server

--- Spotify/Program.cs
+++ Spotify/Program.cs
@@ -17,8 +17,6 @@ namespace Spotify
 {
     class Program
     {
-        static string _ClientId = "b80c989bca714f4b9544319ac76c8c33";
-        static SpotifyWebAPI _spotify;
         /*
           
          Steg ett: Logga in på Spotify
@@ -42,15 +40,16 @@ access_token=BQBbdryq3UCMOoD5BgGRzGkQpd_7queFqzt0ifYMkF6RTIpi-jWAhpv35BWTmbsgG_9
 &expires_in=3600
 &state=         
          */
+
+        //static string _ClientId = "b80c989bca714f4b9544319ac76c8c33";
+        //static SpotifyWebAPI _spotify;
+
         static void Main(string[] args)
         {
-
-            StreamWriter sw = new StreamWriter("spotifydata.json");
-
             var spotify = new SpotifyWebAPI()
             {                
                 TokenType = "Bearer",
-                AccessToken = "BQDyI2OMfVebN0haQovN58UKyQUHQYRNZL94UNTU0U0DxWqurGSsZIRSYoiuMvnlNKgPoS9gpUaXEbFVdQlYJp4lyQpu_F-wSuhtO4ORa3mWTsNanNm1agqhfTV39MIK0bEaKbZ2OafkOWfBUBz_cCHhjf-qe_2E3ro",
+                AccessToken = "BQBPGjGB5WiAn-DvNlP13H9Rsx92neTLpKAIkDjwpdatUfflFrS1VgJmI5-UvXDtmdG7qwcaPcs_cF1sZeZrohegyh274oYbY__S7demnSn4P4SWw_cEgiXgL2E2g8G8vy9Xug4zuwM5FPOx3gTqZCyUXOTh7pjvtVYsGQ",
                 UseAuth = true
             };
 
@@ -60,10 +59,14 @@ access_token=BQBbdryq3UCMOoD5BgGRzGkQpd_7queFqzt0ifYMkF6RTIpi-jWAhpv35BWTmbsgG_9
             if (prof.HasError())
                 Console.WriteLine("err" + prof.Error.Message);
 
-//            Console.WriteLine("Logged in as: " + prof.DisplayName);
-            sw.WriteLine("{ \"userId\": \"" + prof.Id + "\",");
-            sw.WriteLine("  \"username\": \"" + prof.DisplayName + "\",");
-            sw.WriteLine(" \"batches\": [");
+            Console.WriteLine("Logged in as: " + prof.DisplayName);
+            
+            HashSet<SimplePlaylist> Playlists = new HashSet<SimplePlaylist>();
+            //HashSet<CompositePlaylist> Playlists = new HashSet<CompositePlaylist>();
+            HashSet<FullTrack> Tracks = new HashSet<FullTrack>();
+            HashSet<Snapshot> Snapshots = new HashSet<Snapshot>();
+            //            MultiMap<string, string> PlaylistTracks = new MultiMap<string, string>();
+            HashSet<PlaylistTrack> PlaylistTracks = new HashSet<PlaylistTrack>();
 
             bool goOn = true;
             for (int offset = 0; goOn; offset += 20) {
@@ -72,6 +75,7 @@ access_token=BQBbdryq3UCMOoD5BgGRzGkQpd_7queFqzt0ifYMkF6RTIpi-jWAhpv35BWTmbsgG_9
                 if (playlists.HasError()) {
                     if (playlists.Error.Status == 429) {
                         Console.WriteLine("GetUserPlaylists Offset: " + offset + ", RLE, wait " + playlists.Header("Retry-After") + " seconds");
+                        var dummy = playlists.Headers();
                         Thread.Sleep(30 * 1000);
                         playlists = spotify.GetUserPlaylists(userId, 20, offset);
                     } else {
@@ -82,230 +86,109 @@ access_token=BQBbdryq3UCMOoD5BgGRzGkQpd_7queFqzt0ifYMkF6RTIpi-jWAhpv35BWTmbsgG_9
 
                 goOn = playlists.HasNextPage();
 
-                //Console.WriteLine("OK");
-                //Console.WriteLine(playlists.Items.Count + " items");
-                sw.WriteLine(" { \"type\": \"batch\", ");
-                sw.WriteLine("   \"offset\": " + playlists.Offset + ",");
-                sw.WriteLine("   \"count\": " + playlists.Items.Count + ",");
-                sw.WriteLine("   \"lists\": [");
-
                 foreach (var playlist in playlists.Items) {
-                    //Console.WriteLine("  * " + playlist.Name + ", id: " + playlist.Id + ", tracks: " + playlist.Tracks.Total);
-                    var data2 = Newtonsoft.Json.JsonConvert.SerializeObject(playlist);
-                    sw.WriteLine("{ \"type\": \"listcontainer\", ");
-                    sw.WriteLine("  \"list\": " + data2 + ",");
+                    Console.WriteLine("  * " + playlist.Name + ", id: " + playlist.Id + ", tracks: " + playlist.Tracks.Total);
+                    //var data2 = Newtonsoft.Json.JsonConvert.SerializeObject(playlist);
+                    //var p2 = Newtonsoft.Json.JsonConvert.DeserializeObject<CompositePlaylist>(data2);
+                    Playlists.Add(playlist);
+
+                    // Fancy workaround for json-graphql-server's automatic Id matching:
+                    Snapshot snap = new Snapshot() { Id = playlist.SnapshotId };
+                    Snapshots.Add(snap);
 
                     var tracks = spotify.GetPlaylistTracks(userId, playlist.Id);
                     if (tracks.HasError()) {
                         if (tracks.Error.Status == 429) {
                             Console.WriteLine("GetPlaylistTracks " + playlist.Name + ", RLE, wait " + tracks.Header("Retry-After") + " seconds");
-                            Thread.Sleep(30 * 1000);
+                            Thread.Sleep(20 * 1000);
                             tracks = spotify.GetPlaylistTracks(userId, playlist.Id);
                         } else {
                             Console.WriteLine("GetPlaylistTracks " + playlist.Name + ", err " + tracks.Error.Message);
-                            sw.WriteLine(" \"tracks\": null }, ");
                             continue;
                         }
                     }
 
-                    sw.WriteLine("\"tracks\": [");
+                    //var pt = new PlaylistTrack();
+                    //pt.playlist_id = playlist.Id;
+                    //p2.FullTracks = new HashSet<FullTrack>();
+
                     foreach (var track in tracks.Items) {
-                        var data = Newtonsoft.Json.JsonConvert.SerializeObject(track.Track);
-                        sw.WriteLine(data + ",");
+                        //var data = Newtonsoft.Json.JsonConvert.SerializeObject(track.Track);
+                        Tracks.Add(track.Track);
+
+                        var pt = new PlaylistTrack();
+                        pt.playlist_id = playlist.Id;
+                        pt.track_id = track.Track.Id;
+
+                        PlaylistTracks.Add(pt);
+                        //p2.FullTracks.Add( track.Track );
 //                        Console.WriteLine("  * * " + track.Track.Name + " - " + track.Track.Album.Name  + " - " + Artists(track.Track.Artists) + " " + track.Track.PreviewUrl);
                     }
-                    sw.WriteLine("{ \"type\":\"track\" } ]  ");
-                    sw.WriteLine(" }, ");
+
+                    //Playlists.Add(p2);
+                    //PlaylistTracks.Add(pt);
+
                 }
-                sw.WriteLine(" { \"type\": \"listcontainer\" } ]");
-                sw.WriteLine(" },");
             }
-            sw.WriteLine("{ \"type\": \"batch\"} ]");
-            sw.WriteLine("}");
+
+            // Start writing: (tailored for use with https://github.com/marmelab/json-graphql-server)
+            StreamWriter sw = new StreamWriter("spotifydata.js");
+
+            sw.WriteLine("module.exports = { ");
+            sw.WriteLine(" \"snapshots\": ");
+            var data3 = Newtonsoft.Json.JsonConvert.SerializeObject(Snapshots);
+            sw.WriteLine(data3);
+
+            sw.WriteLine(",");
+
+            sw.WriteLine(" \"playlists\": ");
+            data3 = Newtonsoft.Json.JsonConvert.SerializeObject(Playlists);
+            sw.WriteLine(data3);
+
+            sw.WriteLine(",");
+
+            sw.WriteLine(" \"tracks\": ");
+            data3 = Newtonsoft.Json.JsonConvert.SerializeObject(Tracks);
+            sw.WriteLine(data3);
+
+            sw.WriteLine(",");
+
+            sw.WriteLine(" \"playlist_tracks\": ");
+            data3 = Newtonsoft.Json.JsonConvert.SerializeObject(PlaylistTracks);
+            sw.WriteLine(data3);
+
+            sw.WriteLine("};");
 
             sw.Close();
         }
-
-        private static string Artists(List<SimpleArtist> artists)
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (var artist in artists)
-                sb.Append(artist.Name + ", ");
-            var s = sb.ToString();
-            if (s.Length > 2)
-                s = s.Substring(0, s.Length - 2);
-            return s;
-        }
-
-        /*
-        static ImplicitGrantAuth auth;
-        static void Main(string[] args)
-        {
-            //Create the auth object
-            auth = new ImplicitGrantAuth()
-            {
-                //Your client Id
-                ClientId = _ClientId,
-                //Set this to localhost if you want to use the built-in HTTP Server
-                RedirectUri = "http://localhost",
-                //How many permissions we need?
-                Scope = Scope.PlaylistReadPrivate,
-            };
-            //Start the internal http server
-            auth.StartHttpServer();
-            //When we got our response
-            auth.OnResponseReceivedEvent += auth_OnResponseReceivedEvent;
-            //Start
-            auth.DoAuth();
-        }
-
-        static void auth_OnResponseReceivedEvent(Token token, string state) //, string error)
-        {
-            //stop the http server
-            auth.StopHttpServer();
-
-            var spotify = new SpotifyWebAPI()
-            {
-                TokenType = token.TokenType,
-                AccessToken = token.AccessToken
-            };
-
-            var r = spotify.GetUserPlaylists("me");
-            
-            if (r.HasError())
-                Console.WriteLine("err" + r.Error.Message);
-
-            Console.WriteLine("OK");
-            Console.WriteLine(r.Items.Count + " items");
-            //We can now make calls with the token object
-        }
-        */
-        /*
-        static void Main(string[] args)
-        {
-            var p = new Program();
-            p.Run(args);
-        }
-
-        private void Run(string[] args)
-        {
-            DoAuth();
-
-            //var profile = api.GetPrivateProfile();
-            //if (profile.HasError()) {
-            //    var err = profile.Error;
-            //} else {
-            //    var userId = profile.Id;
-
-            //    var lists = api.GetUserPlaylists(userId);
-            //    var x = lists.HasError();
-            //    var y = lists.Items;
-            //    var z = y.Count;
-            //}
-        }
-         */
-        /*
-        private ClientCredentialsAuth auth;
-
-        private SpotifyWebAPI DoAuth()
-        {
-            //Create the auth object
-            auth = new ClientCredentialsAuth()
-            {
-                //Your client Id
-                ClientId = "b80c989bca714f4b9544319ac76c8c33",
-                //Your client secret UNSECURE!!
-                ClientSecret = "72b70cc9e25d4e588e6d38a9934141fe",
-                //How many permissions we need?
-                Scope = Scope.UserReadPrivate,
-            };
-            //With this token object, we now can make calls
-            Token token = auth.DoAuth();
-            var spotify = new SpotifyWebAPI()
-            {
-                TokenType = token.TokenType,
-                AccessToken = token.AccessToken,
-                UseAuth = true
-            };
-
-            return spotify;
-        }
-        */
-        /*
-        static ImplicitGrantAuth a;
-
-        static AutorizationCodeAuth auth;
-        static void DoAuth()
-        {
-            a = new ImplicitGrantAuth();
-            a.ClientId = "b80c989bca714f4b9544319ac76c8c33";
-            a.Scope = Scope.PlaylistReadPrivate;
-            a.RedirectUri = "http://localhost";
-
-            //Create the auth object
-            auth = new AutorizationCodeAuth()
-            {
-                //Your client Id
-                ClientId = "b80c989bca714f4b9544319ac76c8c33",
-                //Set this to localhost if you want to use the built-in HTTP Server
-                RedirectUri = "http://localhost",
-                //How many permissions we need?
-                Scope = Scope.UserReadPrivate,
-            };
-            //This will be called, if the user cancled/accept the auth-request
-            auth.OnResponseReceivedEvent += auth_OnResponseReceivedEvent;
-            //a local HTTP Server will be started (Needed for the response)
-            auth.StartHttpServer();
-            //This will open the spotify auth-page. The user can decline/accept the request
-            auth.DoAuth();
-
-            Thread.Sleep(60000);
-            auth.StopHttpServer();
-            Console.WriteLine("Too long, didnt respond, exiting now...");
-        }
-
-        void dumm7()
-        {
-            OAuthTokenResponse authorizationTokens = OAuthUtility.GetRequestToken(CONSUMER_KEY, CONSUMER_SECRET, "oob");
-
-            string url = String.Format("http://twitter.com/oauth/authorize?oauth_token={0}", authorizationTokens.Token);
-            Console.WriteLine("Go to:\n\n{0}\n\nLogon as @KPCRecipes and enter the pin number below:\n\n", url);
-            string pin = Console.ReadLine();
-
-            OAuthTokenResponse accessTokens = OAuthUtility.GetAccessToken(CONSUMER_KEY, CONSUMER_SECRET, authorizationTokens.Token, pin);
-
-            Console.WriteLine("Here are your access tokens:\n\nScreenName: {0}\nToken: {1}\nTokenSecret: {2}\nUserId: {3}\n\n", accessTokens.ScreenName, accessTokens.Token, accessTokens.TokenSecret, accessTokens.UserId.ToString());
-
-        }
-        private static void auth_OnResponseReceivedEvent(AutorizationCodeAuthResponse response)
-        {
-            //Stop the HTTP Server, done.
-            auth.StopHttpServer();
-
-            //NEVER DO THIS! You would need to provide the ClientSecret.
-            //You would need to do it e.g via a PHP-Script.
-            Token token = auth.ExchangeAuthCode(response.Code, "72b70cc9e25d4e588e6d38a9934141fe");
-
-            var api = new SpotifyWebAPI()
-            {
-                TokenType = token.TokenType,
-                AccessToken = token.AccessToken
-            };
-
-            //With the token object, you can now make API calls
-            var profile = api.GetPrivateProfile();
-            if (profile.HasError()) {
-                var err = profile.Error;
-            } else {
-                var userId = profile.Id;
-
-                var lists = api.GetUserPlaylists(userId);
-                var x = lists.HasError();
-                var y = lists.Items;
-                var z = y.Count;
-            }
-        }
-         */ 
     }
-         
+
+    public class Snapshot
+    {
+        public string Id;
+    }
+
+    public class TrackDTO
+    {
+        public string track_id;
+    }
+
+    public class PlaylistTrack
+    {
+        public PlaylistTrack()
+        {
+            //            track_ids = new HashSet<string>();
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id;
+        public string playlist_id;
+        public string track_id;
+  //      public HashSet<string> track_ids;
+    }
+
+    public class CompositePlaylist: SimplePlaylist
+    {
+        public HashSet<FullTrack> FullTracks;
+    }
 }
